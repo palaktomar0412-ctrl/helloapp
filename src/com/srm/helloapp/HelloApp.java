94d7f831bf58f4e1f2cfa179dd5ee94b01e77276
Palak Tomar <palaktomar0412@gmail.com>
2026-05-05 02:26:37 +0530

    UC2

--- src/com/srm/helloapp/HelloApp.java
+++ src/com/srm/helloapp/HelloApp.java
@@ -3,7 +3,8 @@ package com.srm.helloapp;
 public class HelloApp {
 
 	    public static void main(String[] args) {
-	        System.out.println("Hello World");
+	        String name = args[0];
+	        System.out.println(name);
 	    }
 	
 }
