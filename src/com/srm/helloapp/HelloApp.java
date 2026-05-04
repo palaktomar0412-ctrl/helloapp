04dc567f732be0a0a1c35844f7e0256e6d3f5d7c
Palak Tomar <palaktomar0412@gmail.com>
2026-05-05 02:39:02 +0530

    UC4

--- src/com/srm/helloapp/HelloApp.java
+++ src/com/srm/helloapp/HelloApp.java
@@ -1,13 +1,16 @@
 package com.srm.helloapp;
 
-
 public class HelloApp {
-	public static void main(String[] args) {
-		if (args.length > 0) {
-			System.out.println("Hello " + args[0]);
-	    } else {
-	    	System.out.println("Hello World");
-	    }
-	}
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
+
+    }
 }
 
